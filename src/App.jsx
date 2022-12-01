ad498284eceb63a78cffb382d588d44fa2e1091b
Danko2111 <82421874+Danko2111@users.noreply.github.com>
2022-11-30 22:03:13 -0800

    setup fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <div>
+        <Header />
+      </div>
     </div>
   );
 }
