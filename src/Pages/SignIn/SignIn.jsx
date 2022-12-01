7156c2cd7d08a3b17d6daf447a2f24bb35d6b7d1
Ana Ribas <anaribas@proton.me>
2022-11-30 22:05:37 -0800

    sign in component setup

---
+++ src/Pages/SignIn/SignIn.jsx
@@ -0,0 +1,25 @@
+import './SignIn.scss';
+
+function SignIn() {
+    return (
+        <div className='SignIn'>
+            <div className='SignIn__form-block'>
+                <h1 className='SignIn__primary-text'>Get started with America's largest bookkeeping service</h1>
+                <p className='SignIn__secondary-text'>A Bench financial expert will give you a call and ask a few questions to ensure that Bench is the best fit for your business.</p>
+                <form>
+                    <input className='SignIn__name-input' name='name' placeholder='Full Name'/>
+                    <input className='SignIn__email-input' name='email' type={email} placeholder='Email'/>
+                    <button className='SignIn__form-btn' type='submit'>Start a free trial</button>
+                </form>
+            </div>
+            <div className='SignIn__secondary-block'>
+                <h1>What you get from the call</h1>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default SignIn;
