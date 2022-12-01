6d5823dc896174f06d67068ef107f105a885944c
kiwoonjung <kiwoon0627@gmail.com>
2022-12-01 11:41:48 -0800

    update book page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import SignIn from "./Pages/SignIn/SignIn";
+import Book from "./Pages/Book/Book";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignIn />} />
+          <Route path="/book" element={<Book />} />
         </Routes>
       </BrowserRouter>
     </div>
