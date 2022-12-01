1de61aabbbd30129e44e4a1eff2671a647abed45
Danko2111 <82421874+Danko2111@users.noreply.github.com>
2022-11-30 23:37:39 -0800

    minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/home" element={Home}></Route> */}
-          <Route path="/book" element={Book}></Route>
+          <Route path="/book" element={<Book />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
